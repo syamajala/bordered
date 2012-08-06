48561070cdf3e934c2abdbd3301727a567140084
seshu yamajala <syamajala@gmail.com>
2012-08-05 23:47:05 -0400

    Added punctures to init.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import sage.misc.preparser, os
 from sage.all import *
 
-_bord_prog_files = ["ChainCx.sage", "PMCAlg.sage", "TypeDStr.sage", "TypeDDStr.sage", "Plats.sage"]
+_bord_prog_files = ["ChainCx.sage", "PMCAlg.sage", "TypeDStr.sage", "TypeDDStr.sage", "Plats.sage", "Punctures.sage"]
 
 for _bord_prog_file in _bord_prog_files:
     sage.misc.preparser.load(__path__[0] + os.sep + _bord_prog_file, globals(), attach=False)
